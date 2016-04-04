1263aa1d36f7399d666cc72c7c021cea62123703
Felipe Novaes Medeiros <felipenovaesmedeiros@gmail.com>
2016-04-04 07:40:59 -0300

    Commit Inicial

---
+++ LaraStock/database/migrations/2016_04_04_013345_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('description', 255);
+            $table->decimal('price_cost', 8,2);
+            $table->decimal('price_resale',8,2);
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
